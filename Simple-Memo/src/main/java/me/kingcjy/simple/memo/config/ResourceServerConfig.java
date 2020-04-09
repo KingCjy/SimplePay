4a7889cdb1114a31ca65e7c4a14fa72014899be7
KingCjy <tlssycks@gmail.com>
2020-04-09 18:56:36 +0900

    add: Memo Application Dashboard Authenticate

--- Simple-Memo/src/main/java/me/kingcjy/simple/memo/config/ResourceServerConfig.java
+++ Simple-Memo/src/main/java/me/kingcjy/simple/memo/config/ResourceServerConfig.java
@@ -1,18 +1,9 @@
 package me.kingcjy.simple.memo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableResourceServer
@@ -23,6 +14,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.headers().frameOptions().disable();
         http.authorizeRequests()
                 .antMatchers("/api/*").access("#oauth2.hasScope('read')")
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
     }
 }
